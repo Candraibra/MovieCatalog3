471ebb7952f76e3c1e0bab965fb713703ea2d86e
Candraibra <canisanie@gmail.com>
2019-07-31 11:20:54 +0700

    variable update

--- app/src/main/java/com/candraibra/moviecatalog3/DetailActivity.java
+++ app/src/main/java/com/candraibra/moviecatalog3/DetailActivity.java
@@ -15,6 +15,12 @@ import com.candraibra.moviecatalog3.model.Tv;
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_MOVIE = "extra_movie";
     public static final String EXTRA_TV = "extra_tv";
+    ImageView imgBanner, imgPoster2;
+    RatingBar ratingBar;
+    ProgressBar progressBar;
+    TextView txtTitle, txtDescription, txtRating, txtRealise;
+    String vote, voteReal, banner, poster;
+    float rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,63 +37,63 @@ public class DetailActivity extends AppCompatActivity {
         ProgressBar progressBar = findViewById(R.id.progressBar);
         Movie selectedMovie = getIntent().getParcelableExtra(EXTRA_MOVIE);
         if (selectedMovie != null) {
-            ImageView imgBanner = findViewById(R.id.img_poster);
-            String banner = selectedMovie.getBackdropPath();
-            String poster = selectedMovie.getPosterPath();
+            imgBanner = findViewById(R.id.img_poster);
+            banner = selectedMovie.getBackdropPath();
+            poster = selectedMovie.getPosterPath();
             Glide.with(this)
                     .load(banner)
                     .placeholder(R.drawable.load)
                     .into(imgBanner);
-            ImageView imgPoster2 = findViewById(R.id.img_poster2);
+            imgPoster2 = findViewById(R.id.img_poster2);
             Glide.with(this)
                     .load(poster)
                     .placeholder(R.drawable.load)
                     .into(imgPoster2);
-            TextView txtTitle = findViewById(R.id.txt_title);
+            txtTitle = findViewById(R.id.txt_title);
             txtTitle.setText(selectedMovie.getTitle());
-            TextView txtDescription = findViewById(R.id.txt_description);
-            String vote = Double.toString(selectedMovie.getVoteAverage() / 2);
-            String voteReal = Double.toString(selectedMovie.getVoteAverage());
-            float rating = Float.parseFloat(vote);
-            TextView txtRating = findViewById(R.id.rating);
+            txtDescription = findViewById(R.id.txt_description);
+            vote = Double.toString(selectedMovie.getVoteAverage() / 2);
+            voteReal = Double.toString(selectedMovie.getVoteAverage());
+            rating = Float.parseFloat(vote);
+            txtRating = findViewById(R.id.rating);
             txtRating.setText(voteReal);
-            RatingBar ratingBar = findViewById(R.id.rb_userrating);
+            ratingBar = findViewById(R.id.rb_userrating);
             ratingBar.setRating(rating);
             txtDescription.setText(selectedMovie.getOverview());
-            TextView txtRealise = findViewById(R.id.txt_realise_date);
+            txtRealise = findViewById(R.id.txt_realise_date);
             txtRealise.setText(selectedMovie.getReleaseDate());
             progressBar.setVisibility(View.GONE);
         }
     }
 
     public void setupTv() {
-        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar = findViewById(R.id.progressBar);
         Tv selectedTv = getIntent().getParcelableExtra(EXTRA_TV);
         if (selectedTv != null) {
-            ImageView imgBanner = findViewById(R.id.img_poster);
-            String banner = selectedTv.getBackdropPath();
-            String poster = selectedTv.getPosterPath();
+            imgBanner = findViewById(R.id.img_poster);
+            banner = selectedTv.getBackdropPath();
+            poster = selectedTv.getPosterPath();
             Glide.with(this)
                     .load(banner)
                     .placeholder(R.drawable.load)
                     .into(imgBanner);
-            ImageView imgPoster2 = findViewById(R.id.img_poster2);
+            imgPoster2 = findViewById(R.id.img_poster2);
             Glide.with(this)
                     .load(poster)
                     .placeholder(R.drawable.load)
                     .into(imgPoster2);
-            TextView txtTitle = findViewById(R.id.txt_title);
+            txtTitle = findViewById(R.id.txt_title);
             txtTitle.setText(selectedTv.getName());
-            TextView txtDescription = findViewById(R.id.txt_description);
-            String vote = Double.toString(selectedTv.getVoteAverage() / 2);
-            String voteReal = Double.toString(selectedTv.getVoteAverage());
-            float rating = Float.parseFloat(vote);
-            TextView txtRating = findViewById(R.id.rating);
+            txtDescription = findViewById(R.id.txt_description);
+            vote = Double.toString(selectedTv.getVoteAverage() / 2);
+            voteReal = Double.toString(selectedTv.getVoteAverage());
+            rating = Float.parseFloat(vote);
+            txtRating = findViewById(R.id.rating);
             txtRating.setText(voteReal);
-            RatingBar ratingBar = findViewById(R.id.rb_userrating);
+            ratingBar = findViewById(R.id.rb_userrating);
             ratingBar.setRating(rating);
             txtDescription.setText(selectedTv.getOverview());
-            TextView txtRealise = findViewById(R.id.txt_realise_date);
+            txtRealise = findViewById(R.id.txt_realise_date);
             txtRealise.setText(selectedTv.getFirstAirDate());
             progressBar.setVisibility(View.GONE);
         }
